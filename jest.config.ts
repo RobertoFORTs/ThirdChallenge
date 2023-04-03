03129d3bbc1668890a0dbbe2c865f72147d5da81
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-03 10:23:53 -0400

    build: setting up enviroment

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "@jest/types"
+
+const baseDir = `${__dirname}/./src/**`;
+const baseTestDir = `${__dirname}/./src/test/**`;
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    `${baseDir}/*.ts`
+  ],
+  testMatch: [
+    `${baseTestDir}/*test.ts`
+  ]
+};
+
+export default config;
