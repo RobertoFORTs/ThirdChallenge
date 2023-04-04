7fcef11ea512e3d03b165cd08049dd2d899cdd8a
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-04 18:23:25 -0400

    fix(sessionRoute.ts): fixed import

--- src/routes/sessionRoutes/sessionRoute.ts
+++ src/routes/sessionRoutes/sessionRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { SessionController } from "../../controllers/sessionController/SessionController";
-import { logInValidator } from "../../validators/sessionValidator/loginValidator";
+import { logInValidator } from "../../validators/sessionValidator/logInValidator";
+
 
 const sessionRouter = express.Router();
 
