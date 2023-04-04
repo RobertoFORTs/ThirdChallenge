b15d6db5c53d50709da94fdc21912e68aed58fd9
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-04 10:38:04 -0400

    feat(logInValidator):implement logInValidator

--- src/routes/sessionRoutes/sessionRoute.ts
+++ src/routes/sessionRoutes/sessionRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { SessionController } from "../../controllers/sessionController/SessionController";
+import { logInValidator } from "../../validators/sessionValidator/loginValidator";
 
 const sessionRouter = express.Router();
 
 const sessioncontroller = new SessionController();
 
-sessionRouter.post("/", sessioncontroller.login);
+sessionRouter.post("/", logInValidator, sessioncontroller.login);
 
 export {sessionRouter};
