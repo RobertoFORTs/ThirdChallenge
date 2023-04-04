eca1747029c0a3d39a5db27ffd61922999e25f6b
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-04 14:14:43 -0400

    feat(userRoute): add validators to update route

--- src/routes/userRoutes/userRoute.ts
+++ src/routes/userRoutes/userRoute.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { UserController } from "../../controllers/userController/UserController";
 import { registerUserValidator } from "../../validators/userValidators/registerUserValidator";
+import { idValidator } from "../../validators/idValidator";
+import { updateUserValidator } from "../../validators/userValidators/updateUserValidator";
 
 
 const userRouter = express.Router();
 const userController = new UserController();
 
 userRouter.post("/register", registerUserValidator, userController.registerUser);
-userRouter.put("/update/:id", userController.updateUser);
+userRouter.put("/update/:id", idValidator, updateUserValidator, userController.updateUser);
 
 export {userRouter};
