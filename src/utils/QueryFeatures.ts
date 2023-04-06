140de2d9ddfba4308b95e21e8100fd99e38a223a
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-05 20:00:37 -0400

    fix: fixed random this and refactored for better logic

--- src/utils/QueryFeatures.ts
+++ src/utils/QueryFeatures.ts
@@ -2,14 +2,13 @@
 
 export class QueryFeatures{
 
-  static filter(query: any , queryInfo: object) {
+  static filter(queryInfo: object) {
     const queryObj = { ...queryInfo };
 
     let queryStr = JSON.stringify(queryObj);
     const regex = /\b(gt|gte|lt|lte|in)\b/g; 
     queryStr = queryStr.replace(regex, '$$' + "$1")
-    query = query.find(JSON.parse(queryStr));
-
-    return this;
+  
+    return queryStr;
   }
 }
