7d0941b320a9aeb89cc24d52a4270e8c51f846fe
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-05 23:01:53 -0400

    feat(CarRoute): setting up routes for testting

---
+++ src/routes/carRoutes/carRouter.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { registerUserValidator } from "../../validators/userValidators/registerUserValidator";
+import { idValidator } from "../../validators/idValidator";
+import { SessionController } from "../../controllers/sessionController/SessionController";
+import { CarController } from "../../controllers/carController/CarController";
+
+
+const carRouter = express.Router();
+const carController = new CarController();
+
+carRouter.post("/register", registerUserValidator, carController.registerCar);
+
+carRouter.use(SessionController.authGrantAccess);
+
+carRouter.get("/", carController.getCars);
+carRouter.get("/:id", idValidator, carController.getCarById);
+carRouter.put("/update/:id", idValidator, carController.updateCar);
+carRouter.put("/update/accessory/:id", idValidator, carController.updateAccessory);
+carRouter.delete("/delete/:id", idValidator, carController.deleteCar);
+
+export {carRouter};
