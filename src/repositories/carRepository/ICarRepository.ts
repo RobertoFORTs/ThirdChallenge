9be3b8b7b4c8f2ede62dc82e9e15be9fcb447447
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-06 12:55:35 -0400

    "fix: fixed params"

--- src/repositories/carRepository/ICarRepository.ts
+++ src/repositories/carRepository/ICarRepository.ts
@@ -1,10 +1,11 @@
 import { HydratedDocument } from "mongoose"
 import { ICar } from "../../models/carModel/ICar"
+import { ICreateCarDTO } from "../../dto/CreateCarDTO"
 
 export interface ICarRepository {
   getCars() : Promise<HydratedDocument<ICar>>,
   getCarById() : Promise<HydratedDocument<ICar>>,
-  registerCar() : Promise<HydratedDocument<ICar>>,
+  registerCar(body: ICreateCarDTO) : Promise<HydratedDocument<ICar>>,
   updateCar() : Promise<HydratedDocument<ICar>>,
   updateAccessory() : Promise<HydratedDocument<ICar>>,
   deleteCar() : Promise<void>
