00efd03fd1cf820ba8604b1f2dd542fd7a91d509
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-05 23:04:02 -0400

    feat:(index.ts): finish setting up routes for testing

--- src/routes/carRoutes/carRouter.ts
+++ src/routes/carRoutes/carRouter.ts
@@ -8,12 +8,12 @@ import { CarController } from "../../controllers/carController/CarController";
 const carRouter = express.Router();
 const carController = new CarController();
 
-carRouter.post("/register", registerUserValidator, carController.registerCar);
-
 carRouter.use(SessionController.authGrantAccess);
+
 
 carRouter.get("/", carController.getCars);
 carRouter.get("/:id", idValidator, carController.getCarById);
+carRouter.post("/register", registerUserValidator, carController.registerCar);
 carRouter.put("/update/:id", idValidator, carController.updateCar);
 carRouter.put("/update/accessory/:id", idValidator, carController.updateAccessory);
 carRouter.delete("/delete/:id", idValidator, carController.deleteCar);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRouter } from "./userRoutes/userRoute";
 import { sessionRouter } from "./sessionRoutes/sessionRoute";
+import { carRouter } from "./carRoutes/carRouter";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const baseRoute = "/api/v1";
 
 router.use(`${baseRoute}/user`, userRouter);
 router.use(`${baseRoute}/authenticate`, sessionRouter);
+router.use(`${baseRoute}/car`, carRouter);
 
 export { router };
