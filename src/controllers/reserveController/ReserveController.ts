f76f99431e66e990f3799b710972eb4d1158c837
RobertoFORTs <bobfortesneto@gmail.com>
2023-04-07 14:05:00 -0400

    feat(reserveController):implement of initial ReserveController

---
+++ src/controllers/reserveController/ReserveController.ts
@@ -0,0 +1,86 @@
+import { ReserveRepository } from "../../repositories/reserveRepository/ReserveRepository";
+import { ReserveService } from "../../services/ReserveService";
+import { IReserveController } from "./IReserveController";
+import { Request, Response } from "express";
+
+
+const reserveRepository = new ReserveRepository();
+const reserveService = new ReserveService(reserveRepository);
+
+export class ReserveController implements IReserveController{
+  
+  
+  async getReserves(req: Request, res: Response): Promise<Response> {
+    
+    const queryObj = req.query;
+    const pagination = {
+      page: req.query.page,
+      limit: req.query.limit
+    };
+  
+    const objResponse: object[] | string = await reserveService.executeGetReserves(queryObj, pagination);
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        objResponse
+      }
+    });
+
+  }
+  async getReserveById(req: Request, res: Response): Promise<Response> {
+    
+    const id = req.params.id;
+
+    const user = await reserveService.executeGetReserveById(id);
+
+    return res.status(200).json({
+      status: "success",
+      message: "Reserve found",
+      data: {
+        user
+      }
+    });
+
+  }
+  async registerReserve(req: Request, res: Response): Promise<Response> {
+
+    const newReserve = await reserveService.executeRegister(req.body);
+
+    return res.status(201).json({
+      status: "success",
+      message: "reserve succesfully created",
+      data: {
+        newReserve
+      }
+    });
+
+  }
+  async updateReserve(req: Request, res: Response): Promise<Response> {
+    
+    const objId = {
+      id: req.params.id
+    };
+    Object.assign(req.body, objId);
+    const updatedReserve = await reserveService.executeUpdateReserve(req.body);
+
+    return res.status(200).json({
+      status: "success",
+      message: "reserve has been updated",
+      data: {
+        updatedReserve
+      }
+    });
+
+  }
+  async deleteReserve(req: Request, res: Response): Promise<Response> {
+
+    const id = req.params.id;
+
+    await reserveService.executeDeleteReserve(id);
+
+    return res.status(204).send();
+
+  }
+
+}
