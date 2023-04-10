740f52e83ad0f92388eaa6df0db2cff0f7459799
RobertoFORTs <114432972+RobertoFORTs@users.noreply.github.com>
2023-04-10 11:42:21 -0400

    fix: fix app

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,7 @@ import { ValidationError } from "joi";
 import { connect } from "mongoose";
 import { AppError } from "./errors/AppError";
 import { router } from "./routes";
-import swaggerUI from "swagger-ui-express";
-import swaggerDocs from "./swagger.json";
+
 
 dotenv.config({path: "./config.env"});
 
@@ -14,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/apiDocs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(router);
 
 const DB = process.env.DATABASE!.replace("<PASSWORD>", process.env.DATABASE_PASSWORD!);
